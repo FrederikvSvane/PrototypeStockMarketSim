206a4d8d115da75de5719b1fc7cf02f6a8da00a6
FrederikvSvane <s224766@student.dtu.dk>
2024-01-08 13:30:58 +0100

    update broker class

--- src/main/java/dk/dtu/Broker.java
+++ src/main/java/dk/dtu/Broker.java
@@ -1,17 +1,28 @@
 package dk.dtu;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
 import java.util.UUID;
 import org.jspace.*;
 public class Broker implements Runnable {
 
     String brokerUuid;
-    SequentialSpace requestSpace = new SequentialSpace();
+    SequentialSpace requestSpace;
+    String hostIp;
+    int hostPort;
+    String hostUri;
 
-    public Broker(String brokerUuid){
+    public Broker(String brokerUuid, String hostIp, int hostPort){
         this.brokerUuid = brokerUuid;
         this.requestSpace = new SequentialSpace();
+        this.hostIp = hostIp;
+        this.hostPort = hostPort;
+        this.hostUri = getHostUri("host");
     }
 
+    private String getHostUri(String roomName){
+        return "tcp://" + hostIp + ":" + hostPort + "/" + roomName + "?keep";
+    }
     public Space getSpace(){
         return requestSpace;
     }
@@ -19,11 +30,13 @@ public class Broker implements Runnable {
     public void run() {
         while(true) {
             try {
-                System.out.println("Broker " + brokerUuid + " waiting for order");
                 Object[] request = requestSpace.get(new ActualField(brokerUuid), new FormalField(String.class), new FormalField(Order.class));
                 System.out.println("Broker " + request[0].toString() + " received order" + request[2].toString());
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                RemoteSpace hostSpace = new RemoteSpace(hostUri);
+                hostSpace.put((String) request[0], (String) request[1], (Order) request[2]);
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new RuntimeException("Error in broker");
             }
         }
     }
