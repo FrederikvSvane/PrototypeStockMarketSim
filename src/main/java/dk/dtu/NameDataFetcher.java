72ba768f707279e821f0cec8c6194a26b49fce42
NinjaKnight1 <christoffer.dam.hansen@gmail.com>
2024-01-11 14:00:36 +0100

    DataFetcher Class

---
+++ src/main/java/dk/dtu/NameDataFetcher.java
@@ -0,0 +1,50 @@
+package dk.dtu;
+
+import org.jspace.Space;
+
+import java.io.IOException;
+import java.util.List;
+
+public class NameDataFetcher extends DataFetcher implements Runnable{
+
+    public NameDataFetcher(Space traderDataSpace) {
+        super(traderDataSpace, 1000, "companiesAndPricesHistorySpace");
+    }
+
+    public void run()  {
+        // Tilkobl til Host data space
+        try {
+            connectToDataSpace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        while (true) {
+            // Hent data fra Host data space
+            List<Object[]> companyData;
+            try {
+                companyData = QueryAllCompanies();
+                updateCompanyData(companyData);
+                Thread.sleep(sleepTime);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+    }
+
+
+    @Override
+    void updateCompanyData(List<Object[]> companyData) throws InterruptedException {
+        for(Object[] companyList : companyData) {
+            Company company = (Company) companyList[1];
+            String companyId = company.getCompanyId();
+            String companyName = company.getCompanyName();
+            String companyTicker = company.getCompanyTicker();
+            if(companyNotInTraderSpace(companyId)) {
+                traderDataSpace.put(companyId, companyName, companyTicker);
+            }
+
+        }
+    }
+}
