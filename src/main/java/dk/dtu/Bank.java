ce08df912c842c6c2c9ce50ba7bb000fb0d6c9a9
LucasJuel <lucas.juel.sjostrom@gmail.com>
2024-01-12 22:30:25 +0100

    add history, little issue with missing items in history

--- src/main/java/dk/dtu/Bank.java
+++
@@ -1,8 +0,0 @@
-package dk.dtu;
-
-public class Bank {
-    //space <TraderID, penge, List<Aktier>>
-
-    //PetriNet når man overføre penge og aktier da den kun må overføre penge hvis det er muligt at overfære aktier
-
-}
